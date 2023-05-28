a581b91dedc46d7becf84d55ed8c73918cbac16d
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-05-28 15:52:44 +0200

    commit-1

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -1,7 +1,8 @@
 /* Crea tu propia lógica para hacer un fetch que emule una post request a un servidor y enseñe un mensaje en consola cuando la llamada se resuelva */
 /*  ADVANCED: utiliza DOM manipulation para enseñarle al user que su mensaje se ha enviado correctamente o no */
 const form = document.querySelector(".container-flex-form");
-
+const fullname = document.getElementById('fullname');
+const email = document.getElementById('email');
 
 
 form.addEventListener("submit", function (e) {
@@ -14,11 +15,7 @@ form.addEventListener("submit", function (e) {
     .then((res) => res.json())
     .then((data) => console.log(data))
     .then(() => form.reset())
-    .then(() => {
-        // setTimeout(()=> {
-        //     const message = document.querySelector("message-recive hidden")
-        //     message.classList.remove("hidden")
-        // }, 500)
+    .then(() => {     
         alert("Your message has been recived")
     })
     .catch((err) => {
@@ -28,3 +25,44 @@ form.addEventListener("submit", function (e) {
 });
 
 
+
+// function showError(input, message) {
+//   const formControl = input.parentElement;
+//   formControl.className = 'container-flex-form error';
+//   const small = formControl.querySelector('small');
+//   small.innerText = message;
+// }
+
+// function showSucces(input) {
+//   const formControl = input.parentElement;
+//   formControl.className = 'container-flex-form success';
+// }
+
+// function checkEmail(input) {
+//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+//   if(re.test(input.value.trim())) {
+//       showSucces(input)
+//   }else {
+//       showError(input,'Email is not invalid');
+//   }
+// }
+
+// function checkRequired(inputArr) {
+//   inputArr.forEach(function(input){
+//       if(input.value.trim() === ''){
+//           showError(input,`${getFieldName(input)} is required`)
+//       }else {
+//           showSucces(input);
+//       }
+//   });
+// }
+
+
+// form.addEventListener('submit',function(e) {
+//   e.preventDefault();
+
+//   checkRequired([fullname, email,]);
+//   checkEmail(email);
+ 
+// });
