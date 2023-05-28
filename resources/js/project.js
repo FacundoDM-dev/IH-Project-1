36aa7959b7435e0ff53e01beecf08a94b8c64a1a
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-05-28 12:24:43 +0200

    commit-1

--- resources/js/project.js
+++ resources/js/project.js
@@ -33,7 +33,47 @@ let projectsFetch = fetch("https://raw.githubusercontent.com/ironhack-jc/mid-ter
 }).catch((err) => console.log(err))
 
 
+// const get3posts = () => {
+//     fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
+//     .then((res) => res.json())
+//     .then((res) => {
+//         for (let i =  1; i >= 1; i++) {
+//             document.querySelector(".project-post").innerHTML += 
+//                     `<h1>${res[4].name}</h1>
+//                     <div class="project-post-title-date">
+//                     <h4>${res[4].description}</h4>
+//                     <h5>Completed on 02/02/23</h5>
+//                     </div>
+//                     <div class="img-container">
+//                     </div>
+//                     <p>${res[4].name}</p>`   
+//         }
+//     })
+//     .catch((error) => console.log(error))
 
+//     .then((res) => {
+//         for (let i = res.length - 1; i >= 1; i--) {
+//             document.querySelector('.projects-flex-container').innerHTML += 
+//                     `<div class="project">
+  
+//                       <img src="${res[i].image}" alt="">
+  
+//                       <div id="project" class="project-cont-text">
+  
+//                         <h4>${res[i].name}</h4>
+//                         <p>${res[i].description}</p>
+  
+//                       </div>
+  
+//                       <a href="">Learn more</a>
+  
+//                     </div>`   
+//         }
+//     })
+//     .catch((error) => console.log(error))
+//   }
+  
+//   window.addEventListener("load", get3posts);
 
 
 
