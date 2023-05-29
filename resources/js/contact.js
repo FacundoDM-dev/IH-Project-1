24029398afdb95cc4035df9398a442655ef9a683
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-05-30 01:56:02 +0200

    commit-1

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -1,68 +1,51 @@
 /* Crea tu propia lógica para hacer un fetch que emule una post request a un servidor y enseñe un mensaje en consola cuando la llamada se resuelva */
 /*  ADVANCED: utiliza DOM manipulation para enseñarle al user que su mensaje se ha enviado correctamente o no */
-const form = document.querySelector(".container-flex-form");
-const fullname = document.getElementById('fullname');
-const email = document.getElementById('email');
 
+// FETCH POST ======================>
+
+const form = document.querySelector(".container-flex-form");
+const fullname = document.getElementById("fullname");
+const email = document.getElementById("email");
 
 form.addEventListener("submit", function (e) {
+
   e.preventDefault();
+
   const payload = new FormData(form);
-  fetch("https://httpbin.org/post", {
+
+  fetch("s://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     body: payload,
   })
     .then((res) => res.json())
-    .then((data) => console.log(data))
     .then(() => form.reset())
-    .then(() => {     
-        alert("Your message has been recived")
-    })
-    .catch((err) => {
-        console.log(err)
-        alert("Your message has not been received")
+    .then(() => showSuccess())
+
+    .catch(() => {
+      showError()
     });
 });
 
+// FUNCTIONS FOR DOM MESSAGE ======================>
+
+const showSuccess = (() => {
+  const buttonSubmit = document.querySelector(".button-submit")
+    buttonSubmit.classList.toggle("button-submit-success")
+    buttonSubmit.innerText = "Message received"
+    setInterval(() => {
+      buttonSubmit.classList.remove("button-submit-success")
+      buttonSubmit.innerText = "Submit"
+    }, 2000);
+})
+
+const showError = (() => {
+  const buttonSubmit = document.querySelector(".button-submit")
+    buttonSubmit.classList.toggle("button-submit-error")
+    buttonSubmit.innerText = "Message not received, there was an error"
+    setInterval(() => {
+      buttonSubmit.classList.remove("button-submit-error")
+      buttonSubmit.innerText = "Submit"
+    }, 2000);
+})
 
 
-// function showError(input, message) {
-//   const formControl = input.parentElement;
-//   formControl.className = 'container-flex-form error';
-//   const small = formControl.querySelector('small');
-//   small.innerText = message;
-// }
-
-// function showSucces(input) {
-//   const formControl = input.parentElement;
-//   formControl.className = 'container-flex-form success';
-// }
-
-// function checkEmail(input) {
-//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  
-//   if(re.test(input.value.trim())) {
-//       showSucces(input)
-//   }else {
-//       showError(input,'Email is not invalid');
-//   }
-// }
-
-// function checkRequired(inputArr) {
-//   inputArr.forEach(function(input){
-//       if(input.value.trim() === ''){
-//           showError(input,`${getFieldName(input)} is required`)
-//       }else {
-//           showSucces(input);
-//       }
-//   });
-// }
-
-
-// form.addEventListener('submit',function(e) {
-//   e.preventDefault();
-
-//   checkRequired([fullname, email,]);
-//   checkEmail(email);
- 
-// });
