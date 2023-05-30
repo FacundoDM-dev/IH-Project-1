80b3d1f6b0690fcf871c47d54e0e687f81c4a205
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-05-30 02:04:00 +0200

    commit-1

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -8,44 +8,43 @@ const fullname = document.getElementById("fullname");
 const email = document.getElementById("email");
 
 form.addEventListener("submit", function (e) {
-
   e.preventDefault();
 
   const payload = new FormData(form);
 
-  fetch("s://jsonplaceholder.typicode.com/posts", {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
     method: "POST",
     body: payload,
   })
     .then((res) => res.json())
-    .then(() => form.reset())
-    .then(() => showSuccess())
+    .then(() => {
+      showSuccess();
+      form.reset();
+    })
 
-    .catch(() => {
-      showError()
+    .catch((err) => {
+      showError(err);
     });
 });
 
 // FUNCTIONS FOR DOM MESSAGE ======================>
 
-const showSuccess = (() => {
-  const buttonSubmit = document.querySelector(".button-submit")
-    buttonSubmit.classList.toggle("button-submit-success")
-    buttonSubmit.innerText = "Message received"
-    setInterval(() => {
-      buttonSubmit.classList.remove("button-submit-success")
-      buttonSubmit.innerText = "Submit"
-    }, 2000);
-})
+const showSuccess = () => {
+  const buttonSubmit = document.querySelector(".button-submit");
+  buttonSubmit.classList.toggle("button-submit-success");
+  buttonSubmit.innerText = "Message received";
+  setInterval(() => {
+    buttonSubmit.classList.remove("button-submit-success");
+    buttonSubmit.innerText = "Submit";
+  }, 2000);
+};
 
-const showError = (() => {
-  const buttonSubmit = document.querySelector(".button-submit")
-    buttonSubmit.classList.toggle("button-submit-error")
-    buttonSubmit.innerText = "Message not received, there was an error"
-    setInterval(() => {
-      buttonSubmit.classList.remove("button-submit-error")
-      buttonSubmit.innerText = "Submit"
-    }, 2000);
-})
-
-
+const showError = () => {
+  const buttonSubmit = document.querySelector(".button-submit");
+  buttonSubmit.classList.toggle("button-submit-error");
+  buttonSubmit.innerText = "Message not received, there was an error";
+  setInterval(() => {
+    buttonSubmit.classList.remove("button-submit-error");
+    buttonSubmit.innerText = "Submit";
+  }, 2000);
+};
