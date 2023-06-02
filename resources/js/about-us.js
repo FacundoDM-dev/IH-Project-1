1ef0a7b0328fcd61e1503ef662ba23b01295ed69
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-06-02 17:38:41 +0200

    commit-1

--- resources/js/about-us.js
+++ resources/js/about-us.js
@@ -6,8 +6,8 @@ const getJson = async () => {
     const data = await response.json();
     let post = document.querySelector(".back")
     const dataElement = `<h3>${data.name}</h3>
-                        <h4>${data.age} years.</h4>
-                        <p>${data.profession}, and ${data.aboutme}.</p>`
+                        <h4>${data.age}</h4>
+                        <p>${data.aboutme}</p>`
     post.innerHTML= dataElement                    
 
   } catch {}
