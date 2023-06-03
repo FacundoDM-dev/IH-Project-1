81b12ee8dfd6823aa348e4c35c6f17107e061ec3
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-06-03 14:20:31 +0200

    commit-1

--- resources/js/fetch-one-project-section.js
+++ resources/js/fetch-one-project-section.js
@@ -4,24 +4,44 @@ const get1posts = async () => {
       "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
     );
     const projects = await response.json();
-    const reverseProjects = projects.reverse()
-    let reorderProject = [reverseProjects.pop()].concat(reverseProjects)
-
     const url = new URLSearchParams(window.location.search);
     const numProject = url.get("p");
-
-    const array = reorderProject[numProject];
     let post = document.querySelector(".project-post");
-    
-    const arrayElement = `<h1>${array.name}</h1>
-             <div class="project-post-title-date">
-                 <h4>${array.description}</h4>
-                 <h5>Completed on 02/02/23</h5>
-             </div>
-             <div class="img-container"></div>
-             <p>${array.content}</p>`;
+
+    const projectsSelected = projects.filter((project) => {
+      return project.uuid === numProject;
+    });
+
+    const createHtml = (project) => {
+      return `<h1>${project.name}</h1>
+              <div class="project-post-title-date">
+                  <h4>${project.description}</h4>
+                  <h5>Completed on 02/02/23</h5>
+              </div>
+              <div class="img-container"></div>
+              <p>${project.content}</p>`;
+    };
+
+    const arrayElement = projectsSelected.map((projectSelected) => {
+      return createHtml(projectSelected);
+    });
 
     post.innerHTML = arrayElement;
+
+    // for (let project of projects) {
+    //  if (project.uuid === numProject) {
+    //   const arrayElement = `<h1>${project.name}</h1>
+    //                         <div class="project-post-title-date">
+    //                             <h4>${project.description}</h4>
+    //                             <h5>Completed on 02/02/23</h5>
+    //                         </div>
+    //                         <div class="img-container"></div>
+    //                         <p>${project.content}</p>`;
+
+    // post.innerHTML = arrayElement;
+    // }
+    // }
+
   } catch {
     console.log(error);
   }
